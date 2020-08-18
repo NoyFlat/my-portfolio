b81981de4c0595898827fd9c3cf9bee61c23b827
Noy <noyflat@gmail.com>
2020-08-18 07:14:47 +0000

    Add the Lucy game Add html page for the Lucy game Add photos of Lucy Add video of Lucy Add functions in script to support game Remove random nickname from main page Switch image from main page to video

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const nicknames =
+      ['Efrat', 'Patchpootcheet', 'Patchpoocheeter', 'Cheeter', 'Tamlugeet', 'Pakpukeet', 'FritFruiteet', 'Lucifer', 'Lucinda', 'Nancy Pelucy',
+      'El Monstro', 'Chaplupeet', 'Pupper', 'Puppinda', 'Lucifur', 'Lucy \"The Juice\" Flat Noam'];
+
+const justNames = 
+      ['Chupi', 'Yifat', 'Brurit', 'Pachulina', 'Pepper', 'Pumpkin', 'Mina', 'Lucy \"The Juice\" Noam Flat'];
+
 /**
  * Adds a random nickname to the page.
  */
-function addRandomNickname() {
-  const nicknames =
-      ['Efrat', 'Patchpootcheet', 'Patchpoocheeter', 'Cheeter', 'Tamlugeet', 'Pakpukeet', 'FritFruiteet', 'Lucifer', 'Lucinda', 'Nancy Pelucy',
-      'El Monstro', 'Chaplupeet', 'Pupper', 'Puppinda', 'Lucifur', 'Lucy \"The Juice\" Flat Noam'];
- //Then follow the on-screen instructions to set up your repo.
 
+function addRandomNickname() {
 
   // Pick a random nickname.
   const nickname = nicknames[Math.floor(Math.random() * nicknames.length)];
@@ -28,4 +31,56 @@ function addRandomNickname() {
   // Add it to the page.
   const nicknameContainer = document.getElementById('nickname-container');
   nicknameContainer.innerText = nickname;
+}
+
+function startGame(){
+  //Hide image
+  document.getElementById('image').style = "display:none;";
+ 
+  //Hide response
+   document.getElementById('response-container').style = "display:none;";
+
+  //Pick a random button to show the correct nickname
+  const correctButtonNumber = Math.floor(Math.random() * 3) + 1;
+
+  //Pick a random nickname
+  const nickname = nicknames[Math.floor(Math.random() * nicknames.length)];
+
+  //Pick two different random not-nicknames
+  var notNicknameIndexes = [Math.floor(Math.random() * justNames.length), Math.floor(Math.random() * justNames.length)];
+  while(notNicknameIndexes[0] == notNicknameIndexes[1]){
+    notNicknameIndexes[1] = Math.floor(Math.random() * justNames.length);
+  }
+  var notNicknames = [justNames[notNicknameIndexes[0]], justNames[notNicknameIndexes[1]]];
+
+  //Display and enable button
+  for(var i = 1; i < 4; i++){
+    button = document.getElementById("button"+i);
+    button.style = "";
+    button.disabled = false;
+    if(i == correctButtonNumber){
+      button.innerText = nickname;
+    }
+    else{
+      button.innerText = notNicknames.pop();
+    }
+  }
+}
+
+function generateResponse(button){
+  const responseContainer = document.getElementById('response-container');
+  responseContainer.style = "";
+  if(nicknames.includes(button.innerText)){
+    responseContainer.innerText = "Success!";
+    document.getElementById('image').src = "images/IMG-" + Math.floor(Math.random() * 15) + ".jpg";
+    document.getElementById('image').style = "";
+  }
+  else{
+    responseContainer.innerText = "Sorry, that's not the correct answer";
+  }
+ 
+  //Disable buttons
+  for(var i = 1; i < 4; i++){
+    document.getElementById("button"+i).disabled = true;
+  }
 }
